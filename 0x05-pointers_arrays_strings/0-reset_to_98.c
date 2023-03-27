d0e7683586a547e5bfdcd84291a1f6518880cff6
Tafadzwa Jeff Hove <88307994+Skivyjones366@users.noreply.github.com>
2023-03-27 16:31:57 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,21 +1,12 @@
 #include "main.h"
-
 /**
-
- * reset_to_98 - a function that takes a pointer to an int as parameter
-
- * and updates the value it points to to 98
-
- * @n: input
-
- * Return: n
-
- */
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Owned By Bwave/Bright
+*  Return: 0 is success
+*/
 
 void reset_to_98(int *n)
-
 {
-
-        *n = 98;
-
+	*n = 98;
 }
