8a6a6399847e6144ab67df4facf6fa1766b6099b
Tafadzwa Jeff Hove <88307994+Skivyjones366@users.noreply.github.com>
2023-03-29 22:46:47 +0200

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@ include "main.h"
 /**
  * string_toupper - change lowercase to uppercase
  * @s:string
- * Return:char
+ * Return: char
  */
 char *string_toupper(char *s)
 {
@@ -10,10 +10,10 @@ char *string_toupper(char *s)
 	int i;
 
 i = 0;
-	while (*(s + i))
+	while (s[i] != '\0')
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-			*(s + i) -= 'a' - 'A';
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
 		i++;
 	}
 	return (s);
